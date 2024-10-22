8584b99a0ad35320fc7fde893f9a1f0aa4923b87
Sajid778 <sajidahmed7377@gmail.com>
2024-10-22 22:29:30 +0600

    bookmark and mark as read added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
+const [bookmarks, setBookmarks] = useState([]);
+const [readingTime, setReadingTime] = useState(0)
 
+const handleAddToBookmark = blog => {
+  const newBookmark = [...bookmarks, blog];
+  setBookmarks(newBookmark)
+  
+}
+
+const handleMarkAsRead = time => {
+  console.log('marking as read', time);
+  // setReadingTime(readingTime + time)
+  const newReadingTime = readingTime + time;
+  setReadingTime(newReadingTime);
+  
+}
 
   return (
     <>
    
   <Header></Header>
-  <Blogs></Blogs>
+  
+  <div className='md:flex max-w-7xl mx-auto'>
+  <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+  <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
+  </div>
       
     </>
   )
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,11 @@
-import { data } from "autoprefixer";
+// import { data } from "autoprefixer";
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from "prop-types";
 
 
-const Blogs = () => {
+const Blogs = ({ handleAddToBookmark, handleMarkAsRead }) => {
     const [blogs, setBlogs] = useState([]);
 
    useEffect( () =>{
@@ -12,10 +14,21 @@ const Blogs = () => {
     .then(data => setBlogs(data))
    }, [])
     return (
-        <div>
-            
+        <div className="w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog key={blog.id}
+                     blog={blog}
+                     handleAddToBookmark={handleAddToBookmark}
+                     handleMarkAsRead={handleMarkAsRead}></Blog>)
+            }
         </div>
     );
 };
+
+Blogs.propTypes ={
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
+}
 
 export default Blogs;
